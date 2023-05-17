febf4dc11d8fbaf05b9a6fbfb4f27584401ad717
marlisonmourao <bentesmourao@gmail.com>
2023-05-16 23:15:39 -0400

    home page mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,77 @@
-import { Text, View, StatusBar } from 'react-native'
+import {
+  StatusBar,
+  ImageBackground,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native'
+import { styled } from 'nativewind'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '@assets/bg-blur.png'
+import Stripes from '@assets/stripes.svg'
+import Logo from '@assets/nlwSpaceTime.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Text className="text-4xl font-bold text-zinc-50">Marlison Mourão</Text>
-    </View>
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          className="rounded-full bg-green-500 px-5 py-2"
+          activeOpacity={0.7}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            COMEÇAR A CADASTRAR
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
+      <StyledStripes className="absolute left-2" />
+    </ImageBackground>
   )
 }
